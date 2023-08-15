329878d10b0d8ae06f9666bc5d175a41792e5ef7
Stig <stig@Stigs-MacBook-Pro.local>
2023-08-15 19:49:18 -0300

    test

--- sia-tp0/main.py
+++ sia-tp0/main.py
@@ -105,41 +105,109 @@ if __name__ == "__main__":
     # b)
     # Only using "snorlax" as a pokemon
 
-    columns = ["Pokeballs", "Effect", "Rate", "Health"]    
-    points = 1500
+    # columns = ["Pokeballs", "Effect", "Rate", "Health"]    
+    # points = 1500
+    # healths = [i/points for i in range(points+1)]
+
+    # df2 = pd.DataFrame(columns=columns)
+    # # pokeballs = ["ultraball"]
+    # # StatusEffect = [StatusEffect.NONE]
+
+    # for pokeball in pokeballs:              # 4 pokeballs
+
+    #     for effect in StatusEffect:         # 5 satuseffects
+    #         total_success = 0
+    #         total_try = 0
+
+    #         for healt in healths:           # 4 healths
+
+    #             pokemon = factory.create("snorlax", 100, effect, healt)
+    #             success, iters = run_iteration(pokemon, pokeball, 50, 0)
+    #             total_success += success
+    #             total_try += iters
+    #             new_row = {"Pokeballs": pokeball, "Effect": effect.name, "Rate": total_success/total_try, "Health": healt}
+    #             df2 = df2._append(new_row, ignore_index=True)
+    
+    # # fig = px.bar(df2, x="Pokeballs", y="Rate", color="Effect", barmode="group")
+    # fig = px.scatter(df2, x="Health", y="Rate", color="Pokeballs")
+    
+    # fig.update_layout(title="Success rate only for snorlax with four balls, no effect and different healths")
+
+    # fig.show()
+
+
+    # ---------------------------------------------- #
+
+
+    points = 100
     healths = [i/points for i in range(points+1)]
+    levels_manual = [50, 100, 200]
 
-    df2 = pd.DataFrame(columns=columns)
-    # pokeballs = ["ultraball"]
-    StatusEffect = [StatusEffect.NONE]
+    parameter_grid = {
+        'pokemon': pokemons_name,
+        'effect': StatusEffect,
+        'pokeball': pokeballs,
+        'health': healths,
+        'level': levels_manual
+    }
 
-    for pokeball in pokeballs:              # 4 pokeballs
-
-        for effect in StatusEffect:         # 5 satuseffects
-            total_success = 0
-            total_try = 0
-
-            for healt in healths:           # 4 healths
-
-                pokemon = factory.create("snorlax", 100, effect, healt)
-                success, iters = run_iteration(pokemon, pokeball, 50, 0)
-                total_success += success
-                total_try += iters
-                new_row = {"Pokeballs": pokeball, "Effect": effect.name, "Rate": total_success/total_try, "Health": healt}
-                df2 = df2._append(new_row, ignore_index=True)
+    highest_rate = 0
+    best_params = {}
+    double_best_params = False
     
-    # fig = px.bar(df2, x="Pokeballs", y="Rate", color="Effect", barmode="group")
-    fig = px.scatter(df2, x="Health", y="Rate", color="Pokeballs")
-    
-    fig.update_layout(title="Success rate only for snorlax with four balls, no effect and different healths")
+    columns = ["Pokemon", "Effect", "Rate", "Health", "Pokeballs"]
+    pokemons_manual = ["mewtwo", "caterpie"]
+    # pokemons_manual = ["jolteon", "caterpie", "snorlax", "onix", "mewtwo"]
+
+    df3 = pd.DataFrame(columns=columns)
+
+    for pokemon_for_loop in parameter_grid['level']:
+        for effect in parameter_grid['effect']:
+            for pokeball in parameter_grid['pokeball']:
+                for health in parameter_grid['health']:
+
+                    pokemon = factory.create('mewtwo', 100, effect, health)
+                    success, iters = run_iteration(pokemon, pokeball, pokemon_for_loop, 0)
+                    
+                    iter_success_rate = success/iters
+                    
+                    new_row = {"Pokeballs": pokeball, "Effect": effect.name, "Rate": iter_success_rate, "Health": health, "Pokemon": pokemon.name, "Level": pokemon_for_loop}
+                    df3 = df3._append(new_row, ignore_index=True)
+
+                    if iter_success_rate > highest_rate:
+                        highest_rate = iter_success_rate
+                        best_params = {"Pokeballs": pokeball, "Effect": effect.name, "Rate": iter_success_rate, "Health": health, "Pokemon": pokemon.name, "Level": pokemon_for_loop}
+
+                    if iter_success_rate == highest_rate:
+                        double_best_params = True
+
+
+    print(f"\nSuccess\n{best_params}")
+
+    fig = px.scatter(
+        df3,
+        x="Health",
+        y="Rate",
+        color="Effect",
+        facet_col="Level",  # Creates subplots for each Pokemon
+        facet_row="Pokeballs",  # Creates subplots for each Pokeball
+        category_orders={"Pokemon": pokemons_manual, "Pokeballs": pokeballs},  # Order for facets
+        labels={"Pokemon": "Pokemon", "Pokeballs": "Pokeball"},  # Labels for facet categories
+        trendline="ols",
+    )
+
+    fig.update_layout(
+        title="Influence of Parameters on Success Rate",
+        yaxis_title="Success Rate",
+        xaxis_title="Health",
+        height=1200,  # Set the height of the whole figure
+        width=2000,  # Set the width of the whole figure
+    )
 
     fig.show()
 
+    # plot_html_file = "graphs/2c_scatter_plot2.html"
+    # fig.write_html(plot_html_file)
 
-    # df2.to_csv("data_test_1.csv", index=False)
 
-    # plt.figure(figsize=(10, 10))
-    # #create a scatterplot with health on the x-axis and rate on the y-axis
 
-    # print(df2)
-    # print('nothing happened')
