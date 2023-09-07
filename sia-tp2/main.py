50b6af57079851a3c3b988f211d246e7197f2b2c
William <william.reit@gmail.com>
2023-09-07 02:48:14 -0300

    changes

--- sia-tp2/main.py
+++ sia-tp2/main.py
@@ -1,6 +1,9 @@
 import configparser
 import json
 import numpy as np
+from src.character import Character
+from src.mutation_operators import mutate_population
+
 
 from src.genetic_algorithm import genetic_algorithm
 
@@ -10,28 +13,7 @@ selection_method = 'elite'
 
 result = genetic_algorithm(number_parents, number_iterations, selection_method)
 
-
-# print(result)
-
-
-
-
-
-
-
-
-
-
-# config = configparser.ConfigParser()
-# config.read('config.ini')
-
-# mutation_rate = config.getfloat('GeneticAlgorithm', 'mutation_rate')
-# selection_method = config.get('GeneticAlgorithm', 'selection_method')
-
-# # Chargement des données des personnages depuis le fichier JSON
-# with open('characters_data.json', 'r') as file:
-#     characters_data = json.load(file)
-
-# # Accéder aux caractéristiques d'un personnage
-# warrior_attributes = characters_data['warrior']
-# archer_attributes = characters_data['archer']
+character = Character("warrior", 30, 30, 30, 30, 30, 1.6)
+print(character)
+mutate_population([character], 20, False)
+print(character)
