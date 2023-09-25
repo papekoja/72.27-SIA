99e407c03c2ec744ec92792ac498c02e2b6da430
William <william.reit@gmail.com>
2023-09-25 15:30:47 -0300

    m

--- sia-tp3/src/ej2.py
+++ sia-tp3/src/ej2.py
@@ -4,12 +4,31 @@ from sklearn.model_selection import train_test_split
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv('sia-tp3\data\TP3-ej2-conjunto.csv')
+_, df_test = train_test_split(df, test_size=0.5, random_state=42)
 
-df_train, df_test = train_test_split(df, test_size=0.1, random_state=42)
 
 def run():
+    bootstrap_perceptron = bootstrapping(20, 10)
+    _, bootstrap_mse = bootstrap_perceptron.predict(df_test)
+    print("Bootstrap Perceptron MSE:", bootstrap_mse)
+    random_perceptron = random_selection(0.2)
+    _, random_mse = random_perceptron.predict(df_test)
+    print("Random Perceptron MSE:", random_mse)
+
+def bootstrapping(sample_size, iterations):
+    perceptrons = pd.DataFrame(columns=['w1', 'w2', 'w3', 'b'])
+    for i in range(iterations):
+        temp_df = df.sample(n=sample_size, random_state=42)
+        p = perceptron.Perceptron()
+        p.train(temp_df)
+        w1, w2, w3, b = p.get_weights()
+        perceptrons.loc[len(perceptrons)] = [w1, w2, w3, b]
+    return perceptron.Perceptron()
+
+def random_selection(test_size):
+    df_train, df_test = train_test_split(df, test_size=test_size, random_state=42)
     p = perceptron.Perceptron()
     p.train(df_train)
-    p.predict(df_test)
+    return p
 
 run()
--- sia-tp3/src/perceptron.py
+++ sia-tp3/src/perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class Perceptron:
     
@@ -20,9 +21,15 @@ class Perceptron:
             
 
     def predict(self, df):
+        result = pd.DataFrame(columns=['x1', 'x2', 'x3', 'y', 'y_pred'])
         for row in df.values:
             y_pred = row[0]*self.w1 + row[1]*self.w2 + row[2]*self.w3 + self.b
-            print("x1", row[0], " x2:", row[1], " x3:", row[2], " y:", row[3], " y_pred:", y_pred)
+            result.loc[len(result)] = [row[0], row[1], row[2], row[3], y_pred]
+        # Calculate the Mean Squared Error (MSE)
+        actual = result['y']
+        predicted = result['y_pred']
+        mse = np.mean((actual - predicted)**2)
+        return result, mse
 
     def test(self, df):
         for row in df.values:
@@ -30,3 +37,9 @@ class Perceptron:
 
     def get_weights(self):
         return self.w1, self.w2, self.w3, self.b
+    
+    def set_weights(self, w1, w2, w3, b):
+        self.w1 = w1
+        self.w2 = w2
+        self.w3 = w3
+        self.b = b
