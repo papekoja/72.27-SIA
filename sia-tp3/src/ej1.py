8a02d0a550fb9e9a40c7031436d94d5373d20c51
san1308 <sanchezadrian1308@gmail.com>
2023-09-27 19:56:30 -0300

    ver5

--- sia-tp3/src/ej1.py
+++ sia-tp3/src/ej1.py
@@ -3,35 +3,35 @@ import pandas as pd
 from perceptron import Perceptron
 
 def run():
-    x_and = np.array([[-1, 1, -1], [1, -1, 1], [-1, -1, -1], [1, 1, 1]])
+    x_and = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
     y_and = np.array([-1, -1, -1, 1])
 
-    x_xor = np.array([[-1, 1, -1], [1, -1, 1], [-1, -1, -1], [1, 1, 1]])
+    x_xor = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
     y_xor = np.array([1, 1, -1, -1])
 
     # Crear y entrenar un Perceptrón para la función "Y"
     perceptron_and = Perceptron()
     perceptron_and.non_linear = False  # Usar función de activación escalón
-    df_and = pd.DataFrame(data=np.column_stack((x_and, y_and)), columns=['x1', 'x2', 'x3', 'y'])
-    perceptron_and.train(df_and)
+    df_and = pd.DataFrame(data=np.column_stack((x_and, y_and)), columns=['x1', 'x2', 'y'])
+    perceptron_and.train1(df_and)
 
     # Crear y entrenar un Perceptrón para la función "O exclusivo"
     perceptron_xor = Perceptron()
     perceptron_xor.non_linear = False  # Usar función de activación escalón
-    df_xor = pd.DataFrame(data=np.column_stack((x_xor, y_xor)), columns=['x1', 'x2', 'x3', 'y'])
-    perceptron_xor.train(df_xor)
+    df_xor = pd.DataFrame(data=np.column_stack((x_xor, y_xor)), columns=['x1', 'x2', 'y'])
+    perceptron_xor.train1(df_xor)
 
     # Probar los Perceptrones entrenados
     print("Función lógica 'Y':")
-    perceptron_and.test(df_and)
+    perceptron_and.test1(df_and)
     print("Predicciones para 'Y':")
-    result_and = perceptron_and.predict(df_and)
+    result_and = perceptron_and.predict1(df_and)
     print(result_and)
 
     print("\nFunción lógica 'O exclusivo':")
-    perceptron_xor.test(df_xor)
+    perceptron_xor.test1(df_xor)
     print("Predicciones para 'O exclusivo':")
-    result_xor = perceptron_xor.predict(df_xor)
+    result_xor = perceptron_xor.predict1(df_xor)
     print(result_xor)
 
     # Obtener los pesos de los Perceptrones
@@ -39,3 +39,4 @@ def run():
     print(perceptron_and.get_weights())
     print("\nPesos del Perceptrón para 'O exclusivo':")
     print(perceptron_xor.get_weights())
+
--- sia-tp3/src/perceptron.py
+++ sia-tp3/src/perceptron.py
@@ -37,16 +37,42 @@ class Perceptron:
                 self.w3 += self.lr*(y - y_pred)*row[2]
                 self.b += self.lr*(y - y_pred)
 
+    def train1(self, df):
+        for row in df.values:
+            print(row[2])  # This prints x3
+            x1, x2 = row[0], row[1]
+            y = self.expected_output(row[2])  # Use x3 as input here
+            y_pred = self.output(x1, x2, row[2])  # Use x3 as input here
+            if y_pred != y:
+                self.w1 += self.lr * (y - y_pred) * x1
+                self.w2 += self.lr * (y - y_pred) * x2
+                self.b += self.lr * (y - y_pred)
+
+
+
     def predict(self, df):
         result = pd.DataFrame(columns=['x1', 'x2', 'x3', 'y', 'y_pred'])
         for row in df.values:
             y_pred = row[0]*self.w1 + row[1]*self.w2 + row[2]*self.w3 + self.b
             result.loc[len(result)] = [row[0], row[1], row[2], row[3], y_pred]
         return result
+    
+    def predict1(self, df):
+        result = pd.DataFrame(columns=['x1', 'x2', 'y_pred'])  # Remove 'x3' and 'y' from the column names
+        for row in df.values:
+            y_pred = row[0] * self.w1 + row[1] * self.w2 + self.b  # Remove row[2]*self.w3
+            result.loc[len(result)] = [row[0], row[1], y_pred]  # Remove row[2] and row[3]
+        return result
+
 
     def test(self, df):
         for row in df.values:
             print("x1", row[0], " x2:", row[1], " x3:", row[2], " y:", row[3])
+
+    def test1(self, df):
+        for row in df.values:
+            print("x1:", row[0], " x2:", row[1], " x3:", row[2])
+
 
     def get_weights(self):
         return self.w1, self.w2, self.w3, self.b
