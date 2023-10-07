511c5c682b68d0d332aac756636f3ed8c865916d
san1308 <sanchezadrian1308@gmail.com>
2023-10-07 19:33:53 -0300

    VerRec

--- sia-tp3/src/ej1.py
+++ sia-tp3/src/ej1.py
@@ -1,43 +1,102 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from perceptron import Perceptron
 
-def run():
-    x_and = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-    y_and = np.array([-1, -1, -1, 1])
+# Función para entrenar y probar el perceptrón en un problema lógico
+def train_and_test_logical_function(x, y, function_name):
+    print(f"Entrenando para la función lógica {function_name}")
+    
+    # Crear un DataFrame con los datos de entrada y salida esperada
+    data = pd.DataFrame({'x1': x[:, 0], 'x2': x[:, 1], 'y': y})
+    
+    # Crear una instancia del perceptrón
+    perceptron = Perceptron()
+    
+    # Entrenar el perceptrón
+    perceptron.train1(data)
+    
+    # Realizar predicciones
+    predictions = perceptron.predict1(data)
+    
+    # Imprimir resultados
+    print("Resultados:")
+    print(predictions)
+    print("\nPesos finales del perceptrón:")
+    print(perceptron.get_weights())
 
-    x_xor = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-    y_xor = np.array([1, 1, -1, -1])
+# Definir datos de entrada y salidas esperadas para las funciones lógicas "Y" y "O exclusivo"
+x_and = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+y_and = np.array([-1, -1, -1, 1])
 
-    # Crear y entrenar un Perceptrón para la función "Y"
-    perceptron_and = Perceptron()
-    perceptron_and.non_linear = False  # Usar función de activación escalón
-    df_and = pd.DataFrame(data=np.column_stack((x_and, y_and)), columns=['x1', 'x2', 'y'])
-    perceptron_and.train1(df_and)
+x_xor = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+y_xor = np.array([1, 1, -1, -1])
 
-    # Crear y entrenar un Perceptrón para la función "O exclusivo"
-    perceptron_xor = Perceptron()
-    perceptron_xor.non_linear = False  # Usar función de activación escalón
-    df_xor = pd.DataFrame(data=np.column_stack((x_xor, y_xor)), columns=['x1', 'x2', 'y'])
-    perceptron_xor.train1(df_xor)
+# Entrenar y probar el perceptrón para la función lógica "Y"
+train_and_test_logical_function(x_and, y_and, "Y")
 
-    # Probar los Perceptrones entrenados
-    print("Función lógica 'Y':")
-    perceptron_and.test1(df_and)
-    print("Predicciones para 'Y':")
-    result_and = perceptron_and.predict1(df_and)
-    print(result_and)
+# Entrenar y probar el perceptrón para la función lógica "O exclusivo"
+train_and_test_logical_function(x_xor, y_xor, "O exclusivo")
 
-    print("\nFunción lógica 'O exclusivo':")
-    perceptron_xor.test1(df_xor)
-    print("Predicciones para 'O exclusivo':")
-    result_xor = perceptron_xor.predict1(df_xor)
-    print(result_xor)
 
-    # Obtener los pesos de los Perceptrones
-    print("\nPesos del Perceptrón para 'Y':")
-    print(perceptron_and.get_weights())
-    print("\nPesos del Perceptrón para 'O exclusivo':")
-    print(perceptron_xor.get_weights())
 
-run()
+
+# Función para entrenar y probar el perceptrón en un problema lógico y mostrar gráficas
+def train_and_test_logical_function_with_plots(x, y, function_name):
+    print(f"Entrenando para la función lógica {function_name}")
+    
+    # Crear un DataFrame con los datos de entrada y salida esperada
+    data = pd.DataFrame({'x1': x[:, 0], 'x2': x[:, 1], 'y': y})
+    
+    # Crear una instancia del perceptrón
+    perceptron = Perceptron()
+    
+    # Listas para almacenar los cambios de pesos y errores durante el entrenamiento
+    weight_changes = []
+    errors = []
+    
+    # Entrenar el perceptrón
+    for epoch in range(100):  # Número de épocas de entrenamiento
+        perceptron.train1(data)  # Utiliza train1 para entrenar solo con x1 y x2
+        weight_changes.append(perceptron.get_weights())
+        predictions = perceptron.predict1(data)  # Utiliza predict1 para hacer predicciones con x1 y x2
+        errors.append(np.mean(np.abs(data['y'] - predictions['y_pred'])))  # Compara con 'y' en lugar de 'y_pred'
+    
+    # Crear gráficas
+    plt.figure(figsize=(12, 5))
+    
+    # Gráfica de los cambios en los pesos a lo largo del entrenamiento
+    plt.subplot(1, 2, 1)
+    weight_changes = np.array(weight_changes)
+    plt.plot(weight_changes[:, 0], label='W1')
+    plt.plot(weight_changes[:, 1], label='W2')
+    plt.plot(weight_changes[:, 2], label='B')
+    plt.xlabel('Época')
+    plt.ylabel('Peso')
+    plt.legend()
+    plt.title('Cambios en los pesos durante el entrenamiento')
+    
+    # Gráfica del error a lo largo del entrenamiento
+    plt.subplot(1, 2, 2)
+    plt.plot(errors)
+    plt.xlabel('Época')
+    plt.ylabel('Error')
+    plt.title('Error durante el entrenamiento')
+    
+        # Gráfica de las predicciones
+#    plt.subplot(1, 3, 3)
+#    plt.scatter(data['x1'], data['x2'], c=data['y'], cmap='viridis', label='Real')
+#    plt.scatter(data['x1'], data['x2'], c=predictions['y_pred'], cmap='viridis', marker='x', label='Predicción')
+#    plt.xlabel('x1')
+#   plt.ylabel('x2')
+#   plt.legend()
+#    plt.title('Predicciones')
+#
+    plt.tight_layout()
+    plt.show()
+
+# Entrenar y probar el perceptrón para la función lógica "Y" con gráficas
+train_and_test_logical_function_with_plots(x_and, y_and, "Y")
+
+# Entrenar y probar el perceptrón para la función lógica "O exclusivo" con gráficas
+train_and_test_logical_function_with_plots(x_xor, y_xor, "O exclusivo")
--- sia-tp3/src/perceptron.py
+++ sia-tp3/src/perceptron.py
@@ -17,6 +17,13 @@ class Perceptron:
         else:
             return x1*self.w1 + x2*self.w2 + x3*self.w3 + self.b
         
+    def output1(self, x1, x2):
+        if self.non_linear:
+            return self.sigmoid(x1*self.w1 + x2*self.w2 + self.b)
+        else:
+            return x1*self.w1 + x2*self.w2 + self.b
+
+        
     def expected_output(self, y):
         if self.non_linear:
             return self.sigmoid(y)
@@ -38,14 +45,12 @@ class Perceptron:
 
     def train1(self, df):
         for row in df.values:
-            print(row[2])  # This prints x3
-            x1, x2 = row[0], row[1]
-            y = self.expected_output(row[2])  # Use x3 as input here
-            y_pred = self.output(x1, x2, row[2])  # Use x3 as input here
+            y = self.expected_output(row[2])  # Aquí usamos x3
+            y_pred = self.output1(row[0], row[1])  # Aquí omitimos x3
             if y_pred != y:
-                self.w1 += self.lr * (y - y_pred) * x1
-                self.w2 += self.lr * (y - y_pred) * x2
-                self.b += self.lr * (y - y_pred)
+                self.w1 += self.lr*(y - y_pred)*row[0]
+                self.w2 += self.lr*(y - y_pred)*row[1]
+                self.b += self.lr*(y - y_pred)
 
 
 
@@ -57,10 +62,10 @@ class Perceptron:
         return result
     
     def predict1(self, df):
-        result = pd.DataFrame(columns=['x1', 'x2', 'y_pred'])  # Remove 'x3' and 'y' from the column names
+        result = pd.DataFrame(columns=['x1', 'x2', 'y_pred'])  
         for row in df.values:
-            y_pred = row[0] * self.w1 + row[1] * self.w2 + self.b  # Remove row[2]*self.w3
-            result.loc[len(result)] = [row[0], row[1], y_pred]  # Remove row[2] and row[3]
+            y_pred = row[0] * self.w1 + row[1] * self.w2 + self.b  
+            result.loc[len(result)] = [row[0], row[1], y_pred] 
         return result
 
 
