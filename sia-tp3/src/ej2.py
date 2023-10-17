6d38b55ea60ed46aea4064334d0aa93ebad2e7a4
Stig <stig@Stigs-MacBook-Pro.local>
2023-10-17 03:21:41 -0300

    commit

--- sia-tp3/src/ej2.py
+++ sia-tp3/src/ej2.py
@@ -6,145 +6,98 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import LeaveOneOut
 
-def bootstrapping(sample_size, iterations):
-    perceptrons = pd.DataFrame(columns=['w1', 'w2', 'w3', 'b'])
-    for _ in range(iterations):
-        temp_df = df.sample(n=sample_size, random_state=42)
-        p = perceptron.Perceptron()
-        p.train(temp_df)
-        w1, w2, w3, b = p.get_weights()
-        perceptrons.loc[len(perceptrons)] = [w1, w2, w3, b]
-    p = perceptron.Perceptron()
-    p.set_weights(np.mean(perceptrons['w1']), np.mean(perceptrons['w2']), np.mean(perceptrons['w3']), np.mean(perceptrons['b']))
-    return p
-
-def random_selection(test_size):
-    df_train, df_test = train_test_split(df, test_size=test_size, random_state=42)
-    p = perceptron.Perceptron()
-    p.train(df_train)
-    return p
-
-def k_fold_cross_validation(k):
-    perceptrons = pd.DataFrame(columns=['w1', 'w2', 'w3', 'b'])
-    kf = KFold(n_splits=k, shuffle=False, random_state=None)  # Use a fixed random_state for reproducibility
-    for train_index, test_index in kf.split(df):
-        p = perceptron.Perceptron()
-        p.train(df.iloc[train_index])
-        w1, w2, w3, b = p.get_weights()
-        perceptrons.loc[len(perceptrons)] = [w1, w2, w3, b]
-    p = perceptron.Perceptron()
-    p.set_weights(np.mean(perceptrons['w1']), np.mean(perceptrons['w2']), np.mean(perceptrons['w3']), np.mean(perceptrons['b']))
-    return p
-
-def leave_one_out_cross_validation():
-    perceptrons = pd.DataFrame(columns=['w1', 'w2', 'w3', 'b'])
-    loo = LeaveOneOut()
-    for train_index, test_index in loo.split(df):
-        p = perceptron.Perceptron()
-        p.train(df.iloc[train_index])
-        w1, w2, w3, b = p.get_weights()
-        perceptrons.loc[len(perceptrons)] = [w1, w2, w3, b]
-    p = perceptron.Perceptron()
-    p.set_weights(np.mean(perceptrons['w1']), np.mean(perceptrons['w2']), np.mean(perceptrons['w3']), np.mean(perceptrons['b']))
-    return p
-
-#Evaluate the performance of the perceptron with MSE(Mean Square Error) and MAE(Mean Absolute Error)
-def evaluate(actual, predicted):
-    mse = mean_squared_error(actual, predicted)
-    mae = mean_absolute_error(actual, predicted)
-    return mse, mae
-
-#Plot grapg with two different y-axis for MSE and MAE with shared x-axis
-def plot_graph(title, y1, y2, x):
-    # Create a figure and axis for the first curve
-    fig, ax1 = plt.subplots()
-    # Plot the first curve on the left y-axis
-    ax1.plot(x, y1, label='MSE', color='b', marker='o', markersize=5)
-    # Set labels and title for the left y-axis
-    ax1.set_xlabel('X-axis')
-    ax1.set_ylabel('MSE-axis', color='b')
-    ax1.tick_params(axis='y', labelcolor='b')
-    # Create a second y-axis (right y-axis) sharing the same x-axis
-    ax2 = ax1.twinx()
-    # Plot the second curve on the right y-axis
-    ax2.plot(x, y2, label='MAE', color='r', linestyle='--', marker='s', markersize=5)
-    # Set labels and title for the right y-axis
-    ax2.set_ylabel('MAE-axis', color='r')
-    ax2.tick_params(axis='y', labelcolor='r')
-    # Add a legend for both curves
-    lines1, labels1 = ax1.get_legend_handles_labels()
-    lines2, labels2 = ax2.get_legend_handles_labels()
-    lines = lines1 + lines2
-    labels = labels1 + labels2
-    ax1.legend(lines, labels, loc='upper left')
-    # Show the plot
-    plt.title(title)
-    plt.grid(True)
-    plt.show()
-
-#train the perceptrons with bootstrapping with different sample sizes ranging from 2 to 22 and plot the performance on a graph
-def bootstrapping_evaluate():
-    evalueations = []
-    intervals = [i for i in range(2, 11, 2)]
-    for i in intervals:
-        p = bootstrapping(i, 10)
-        evalueations.append(evaluate(df_test['y'], p.predict(df_test)['y_pred']))
-    y1 = [i[0] for i in evalueations]
-    y2 = [i[1] for i in evalueations]
-    plot_graph("Bootstrapping" ,y1, y2, intervals)
-
-#train the perceptrons with random selection with different test sizes ranging from 0.1 to 0.9 and plot the performance on a graph
-def random_selection_evaluate():
-    evalueations = []
-    intervals = [i/10 for i in range(1, 10)]
-    for i in intervals:
-        p = random_selection(i)
-        evalueations.append(evaluate(df_test['y'], p.predict(df_test)['y_pred']))
-    y1 = [i[0] for i in evalueations]
-    y2 = [i[1] for i in evalueations]
-    plot_graph("Random Selection", y1, y2, intervals)
-
-#train the perceptrons with k-fold cross validation with different k values ranging from 2 to 10 and plot the performance on a graph
-def k_fold_cross_validation_evaluate():
-    evalueations = []
-    intervals = [i for i in range(2, 11, 2)]
-    for i in intervals:
-        p = k_fold_cross_validation(i)
-        evalueations.append(evaluate(df_test['y'], p.predict(df_test)['y_pred']))
-    y1 = [i[0] for i in evalueations]
-    y2 = [i[1] for i in evalueations]
-    plot_graph("K-Fold Cross Validation", y1, y2, intervals)
-
-#train the perceptrons with leave one out cross validation and plot the performance on a graph
-def leave_one_out_cross_validation_evaluate():
-    p = leave_one_out_cross_validation()
-    mse, mae = evaluate(df_test['y'], p.predict(df_test)['y_pred'])
-    plot_graph("Leave One Out Cross Validation", [mse], [mae], [1])
-
 def mean_squared_error(actual, predicted):
     return np.mean((actual - predicted)**2)
 
 def mean_absolute_error(actual, predicted):
     return np.mean(np.abs(actual - predicted))
 
-def plot_learning_curve(self):
-    plt.plot(self.training_error_history)
+def plot_learning_curve(training_error_history):
+    plt.plot(training_error_history)
     plt.xlabel('Epoch')
     plt.ylabel('MSE')
     plt.title('Learning Curve')
     plt.show()
-
-# print the current path
-import os
-print(os.getcwd())
     
 # Read the CSV file into a DataFrame
 df = pd.read_csv('sia-tp3/data/TP3-ej2-conjunto.csv')
-df_train, df_test = train_test_split(df, test_size=0.5, random_state=42)
 
-perceptron = perceptron.Perceptron()
-perceptron.non_linear = False
-perceptron.train(df_train, epochs=100)
+# Normalize the features (x1, x2, x3)
+feature_cols = ['x1', 'x2', 'x3']
+df[feature_cols] = (df[feature_cols] - df[feature_cols].min()) / (df[feature_cols].max() - df[feature_cols].min())
 
-# Plot the learning curve
-perceptron.plot_learning_curve()
+# Normalize the target (y)
+df['y'] = (df['y'] - df['y'].min()) / (df['y'].max() - df['y'].min())
+
+# pt create learning rate graphs
+""" for i in range(1, 11):
+    p = perceptron.Perceptron(0.001*i, 1)
+    p.non_linear = True
+    p.train(df_train, epochs=100)
+    plt.plot(range(1, len(p.training_error_history) + 1), p.training_error_history, label=f"Learning rate = {0.001*i}")
+
+plt.xlabel('Epoch')
+plt.ylabel('MSE')
+plt.title('Error over Epochs for Different Learning Rates')
+plt.legend()    
+plt.show() """
+
+# create beta 3d graph
+
+""" # Train the perceptron with varying learning rates and beta values
+learning_rates = np.linspace(0.001, 0.1, 10)  # for example
+beta_values = np.linspace(0.5, 5, 10)        # for example
+errors = np.zeros((len(learning_rates), len(beta_values)))
+
+for i, lr in enumerate(learning_rates):
+    for j, beta in enumerate(beta_values):
+        p = perceptron.Perceptron(lr, beta)
+        p.non_linear = True
+        p.learning_rate = lr
+        p.train(df_train, epochs=100)
+        # calculate the average error from error history
+        #error = np.mean(p.training_error_history)
+        #calculate the mean squared error from test data
+        y_pred = p.output(df_test.iloc[:, 0].values, df_test.iloc[:, 1].values, df_test.iloc[:, 2].values)
+        error = mean_squared_error(df_test.iloc[:, 3].values, y_pred)
+        errors[i, j] = error
+
+# Plot the results on a 3D graph
+X, Y = np.meshgrid(learning_rates, beta_values)
+Z = errors.T  # transpose the error matrix to align with meshgrid
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+ax.set_xlabel('Learning Rate')
+ax.set_ylabel('Beta Value')
+ax.set_zlabel('Error')
+ax.set_title('Error for different Learning Rates and Beta Values')
+
+plt.show() """
+
+# 2.2 Compare training and test error for different test sizes
+
+# Split the data into training and test sets
+
+test_sizes = np.linspace(0.1, 0.9, 9)
+training_errors = []
+test_errors = []
+
+for test_size in test_sizes:
+    df_train, df_test = train_test_split(df, test_size=test_size)
+    p = perceptron.Perceptron(0.06, 3)
+    p.non_linear = True
+    p.train(df_train, epochs=100)
+    training_errors.append(p.training_error_history[-1])
+    y_pred = p.output(df_test.iloc[:, 0].values, df_test.iloc[:, 1].values, df_test.iloc[:, 2].values)
+    test_errors.append(mean_squared_error(df_test.iloc[:, 3].values, y_pred))
+
+plt.plot(test_sizes, training_errors, label='Training Error')
+plt.plot(test_sizes, test_errors, label='Test Error')
+plt.xlabel('Test Size')
+plt.ylabel('MSE')
+plt.title('Training and Test Error for Different Test Sizes')
+plt.legend()
+plt.show()
--- sia-tp3/src/perceptron.py
+++ sia-tp3/src/perceptron.py
@@ -4,14 +4,14 @@ import pandas as pd
 class Perceptron:
     non_linear = False
     
-    def __init__(self):
+    def __init__(self, lr, beta):
         self.w1 = np.random.rand()
         self.w2 = np.random.rand()
         self.w3 = np.random.rand()
         self.b = np.random.rand()
-        self.lr = 0.5
+        self.lr = lr
+        self.beta = beta
         self.training_error_history = []
-
 
     def output(self, x1, x2, x3):
         if self.non_linear:
@@ -33,16 +33,16 @@ class Perceptron:
             return y
 
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        return 1 / (1 + np.exp(-self.beta*x))
 
     def sigmoid_derivative(self, x):
-        return x * (1 - x)
+        return self.beta * x * (1 - x)
     
     def mean_squared_error(self, actual, predicted):
         return np.mean((actual - predicted)**2)
     
     def train(self, df, epochs, tol=1e-5, patience=5):
-        errors = []
+        self.training_error_history = []
         no_improve = 0  # Counter for epochs with no improvement
         for i in range(epochs):
             if self.non_linear:
@@ -50,15 +50,16 @@ class Perceptron:
             else:
                 self.train_linear(df)
 
+            y_pred = self.output(df.iloc[:, 0].values, df.iloc[:, 1].values, df.iloc[:, 2].values)
             # Assuming df has input features in first 3 columns and output in the 4th.
             loss = self.mean_squared_error(df.iloc[:, 3].values, y_pred)
             
-            errors.append(loss)
+            self.training_error_history.append(loss)
             
             # Check if we should stop early
             if i > 0:
                 # Compare current loss to previous loss
-                if abs(errors[-1] - errors[-2]) < tol:
+                if abs(self.training_error_history[-1] - self.training_error_history[-2]) < tol:
                     no_improve += 1
                 else:  # Reset counter if loss improved
                     no_improve = 0
@@ -71,29 +72,23 @@ class Perceptron:
             print(f"Training finished: Maximum epochs ({epochs}) reached.")
     
     def train_linear(self, df):
-        self.training_error_history = []
         for row in df.values:
-            y = self.expected_output(row[3])
+            y = row[3]
             y_pred = self.output(row[0], row[1], row[2])
-            self.w1 -= self.lr*(y - y_pred)*row[0]
-            self.w2 -= self.lr*(y - y_pred)*row[1]
-            self.w3 -= self.lr*(y - y_pred)*row[2]
-            self.b -= self.lr*(y - y_pred)
-            mse = self.mean_squared_error(y, y_pred)
-            self.training_error_history.append(mse)
+            self.w1 += self.lr*(y - y_pred)*row[0]
+            self.w2 += self.lr*(y - y_pred)*row[1]
+            self.w3 += self.lr*(y - y_pred)*row[2]
+            self.b += self.lr*(y - y_pred)
 
 
     def train_non_linear(self, df):
-        self.training_error_history = []
         for row in df.values:
-            y = self.expected_output(row[3])
+            y = row[3]
             y_pred = self.output(row[0], row[1], row[2])
-            self.w1 -= self.lr*(y - y_pred)*row[0]*self.sigmoid_derivative(y_pred)
-            self.w2 -= self.lr*(y - y_pred)*row[1]*self.sigmoid_derivative(y_pred)
-            self.w3 -= self.lr*(y - y_pred)*row[2]*self.sigmoid_derivative(y_pred)
-            self.b -= self.lr*(y - y_pred)*self.sigmoid_derivative(y_pred)
-            mse = self.mean_squared_error(y, y_pred)
-            self.training_error_history.append(mse)
+            self.w1 += self.lr*(y - y_pred)*row[0]*self.sigmoid_derivative(y_pred)
+            self.w2 += self.lr*(y - y_pred)*row[1]*self.sigmoid_derivative(y_pred)
+            self.w3 += self.lr*(y - y_pred)*row[2]*self.sigmoid_derivative(y_pred)
+            self.b += self.lr*(y - y_pred)*self.sigmoid_derivative(y_pred)
 
     def train1(self, df):
         for row in df.values:
